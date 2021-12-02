164beff470480d744cf56211f76503762c7ad0d3
dgoldsb <dgoldsb@live.nl>
2021-12-02 08:20:38 +0100

    Day 2 solutions

---
+++ days/src/day_02.rs
@@ -0,0 +1,67 @@
+use aoc::parse_lines;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+enum Operation {
+    Up(usize),
+    Down(usize),
+    Forward(usize),
+}
+
+impl FromStr for Operation {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Operation, Self::Err> {
+        let mut split = input.split(" ");
+        let op = split.next().unwrap();
+        let val: usize = split.next().unwrap().parse().unwrap();
+        match op {
+            "up" => Ok(Operation::Up(val)),
+            "down" => Ok(Operation::Down(val)),
+            "forward" => Ok(Operation::Forward(val)),
+            _ => Err(()),
+        }
+    }
+}
+
+fn part_a(operations: &Vec<Operation>) -> usize {
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+
+    for operation in operations {
+        match operation {
+            Operation::Forward(i) => x += i,
+            Operation::Down(i) => y += i,
+            Operation::Up(i) => y -= i,
+        }
+    }
+    x * y
+}
+
+fn part_b(operations: &Vec<Operation>) -> usize {
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut aim: usize = 0;
+
+    for operation in operations {
+        match operation {
+            Operation::Forward(i) => {
+                x += i;
+                y += aim * i;
+            }
+            Operation::Down(i) => aim += i,
+            Operation::Up(i) => aim -= i,
+        }
+    }
+    x * y
+}
+
+pub fn day_02() {
+    let instructions = parse_lines("day_02".to_string());
+    let operations = instructions
+        .iter()
+        .map(|i| Operation::from_str(i).unwrap())
+        .collect();
+    println!("A: {}", part_a(&operations));
+    println!("B: {}", part_b(&operations));
+}
--- runner/main.rs
+++ runner/main.rs
@@ -1,4 +1,5 @@
 use days::day_01::day_01;
+use days::day_02::day_02;
 use std::env;
 
 fn main() {
@@ -7,5 +8,10 @@ fn main() {
     if args.contains(&"all".to_string()) || args.contains(&"1".to_string()) {
         println!("Running day {}", 1);
         day_01();
+    }
+
+    if args.contains(&"all".to_string()) || args.contains(&"2".to_string()) {
+        println!("Running day {}", 2);
+        day_02();
     }
 }
