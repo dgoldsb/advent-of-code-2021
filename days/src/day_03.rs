d10a52c9ef94801d2aa2f58b9c27264d644a08ef
dgoldsb <dgoldsb@live.nl>
2021-12-03 15:25:23 +0100

    Part 2 of day 3

--- days/src/day_03.rs
+++ days/src/day_03.rs
@@ -1,13 +1,18 @@
 use aoc::from_bin;
 use aoc::parse_lines;
+use std::collections::HashSet;
 
 fn tally_to_binary_char(number: &isize, invert: bool) -> char {
-    if (number > &0) ^ !invert {
+    if (number == &0) && invert {
+        '1'
+    } else if number == &0 {
+        '0'
+    } else if (number > &0) ^ !invert {
         '1'
     } else if (number < &0) ^ !invert {
         '0'
     } else {
-        panic!("drew even!")
+        panic!("unknown case")
     }
 }
 
@@ -26,8 +31,6 @@ fn part_a(instructions: &Vec<String>) -> usize {
         }
     }
 
-    println!("{:?}", array);
-
     let gamma = from_bin(
         &array
             .iter()
@@ -43,8 +46,55 @@ fn part_a(instructions: &Vec<String>) -> usize {
     gamma * epsilon
 }
 
+fn recursive_filter(instruction_set: &HashSet<String>, index: usize, invert: bool) -> String {
+    let mut tally: isize = 0;
+
+    for instruction in instruction_set {
+        match instruction.chars().nth(index).unwrap() {
+            '1' => tally += 1,
+            '0' => tally -= 1,
+            _ => {
+                panic!("unknown case")
+            }
+        }
+    }
+
+    let target_char = tally_to_binary_char(&tally, invert);
+    let mut new_set: HashSet<String> = HashSet::new();
+
+    for instruction in instruction_set {
+        if instruction.chars().nth(index).unwrap() == target_char {
+            new_set.insert(instruction.to_string());
+        }
+    }
+
+    if new_set.len() == 1 {
+        new_set.iter().next().unwrap().clone()
+    } else {
+        recursive_filter(&new_set, index + 1, invert)
+    }
+}
+
+fn part_b(instructions: &Vec<String>) -> usize {
+    let instruction_set: HashSet<String> = instructions
+        .iter()
+        .map(|s| s.clone())
+        .collect::<HashSet<String>>();
+    let oxygen = from_bin(
+        &recursive_filter(&instruction_set, 0, true)
+            .chars()
+            .collect::<Vec<char>>(),
+    );
+    let co2 = from_bin(
+        &recursive_filter(&instruction_set, 0, false)
+            .chars()
+            .collect::<Vec<char>>(),
+    );
+    oxygen * co2
+}
+
 pub fn day_03() {
     let instructions = parse_lines("day_03".to_string());
     println!("A: {}", part_a(&instructions));
-    println!("B: {}", part_a(&instructions));
+    println!("B: {}", part_b(&instructions));
 }
