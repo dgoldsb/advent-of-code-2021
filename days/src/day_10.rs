6ae19c99d4301a2ef21dae68966f54f781638514
dgoldsb <dgoldsb@live.nl>
2021-12-10 10:57:22 +0100

    Day 10 part 2

--- days/src/day_10.rs
+++ days/src/day_10.rs
@@ -4,7 +4,7 @@ use std::collections::VecDeque;
 const OPENS: [char; 4] = ['<', '{', '[', '('];
 const CLOSES: [char; 4] = ['>', '}', ']', ')'];
 
-fn calculate_line_corruption(line: &String) -> usize {
+fn calculate_line_value(line: &String) -> isize {
     let mut stack: VecDeque<char> = VecDeque::new();
     for ch in line.chars() {
         if OPENS.contains(&ch) {
@@ -27,15 +27,40 @@ fn calculate_line_corruption(line: &String) -> usize {
             }
         }
     }
-    0
+    let mut value = 0;
+    while !stack.is_empty() {
+        value *= 5;
+        value += match stack.pop_back().unwrap() {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            _ => 0,
+        }
+    }
+    value * -1
 }
 
-fn solve_a(lines: &Vec<String>) -> usize {
-    lines.iter().map(|l| calculate_line_corruption(l)).sum()
+fn solve_a(lines: &Vec<String>) -> isize {
+    lines
+        .iter()
+        .map(|l| calculate_line_value(l))
+        .filter(|v| *v > 0)
+        .sum()
+}
+
+fn solve_b(lines: &Vec<String>) -> isize {
+    let mut values = lines
+        .iter()
+        .map(|l| calculate_line_value(l))
+        .filter(|v| *v < 0)
+        .collect::<Vec<isize>>();
+    values.sort();
+    values.get(values.len() / 2).unwrap() * -1
 }
 
 pub fn day_10() {
     let input = parse_lines("day_10".to_string());
     println!("A: {}", solve_a(&input));
-    println!("B: {}", solve_a(&input));
+    println!("B: {}", solve_b(&input));
 }
