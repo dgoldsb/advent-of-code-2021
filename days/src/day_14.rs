a50ba4e829c502011ca05feb83e6ff6b64f4d16f
dgoldsb <dgoldsb@live.nl>
2021-12-14 11:30:44 +0100

    Day 14, now fast

--- days/src/day_14.rs
+++ days/src/day_14.rs
@@ -15,31 +15,65 @@ fn parse_rules(input: &String) -> HashMap<(char, char), char> {
 
 fn solve(template: &String, rules: &HashMap<(char, char), char>, part_a: bool) -> usize {
     let mut current_string = template.chars().collect::<Vec<char>>();
+    current_string.push('x'); // mark the tail
+
+    let mut window_count: HashMap<(char, char), usize> = HashMap::new();
+    current_string.windows(2).for_each(|w| {
+        let t = (w[0], w[1]);
+        match window_count.get_mut(&t) {
+            Some(v) => *v += 1,
+            _ => {
+                window_count.insert(t, 1);
+            }
+        };
+    });
 
     for i in 0..40 {
         if i == 10 && part_a {
             break;
         }
 
-        current_string.push('-');
-        current_string = current_string
-            .windows(2)
-            .map(|t| match rules.get(&(t[0], t[1])) {
-                Some(v) => vec![t[0], *v],
-                _ => vec![t[0]],
-            })
-            .fold(Vec::new(), |mut v, a| {
-                v.extend(a);
-                v
-            });
+        let mut new_window_count: HashMap<(char, char), usize> = HashMap::new();
 
+        for (t, c) in window_count {
+            match rules.get(&t) {
+                Some(v) => {
+                    let t1 = (t.0, *v);
+                    let t2 = (*v, t.1);
+                    match new_window_count.get_mut(&t1) {
+                        Some(v) => *v += c,
+                        _ => {
+                            new_window_count.insert(t1, c);
+                        }
+                    };
+                    match new_window_count.get_mut(&t2) {
+                        Some(v) => *v += c,
+                        _ => {
+                            new_window_count.insert(t2, c);
+                        }
+                    };
+                }
+                _ => {
+                    match new_window_count.get_mut(&t) {
+                        Some(v) => *v += c,
+                        _ => {
+                            new_window_count.insert(t, c);
+                        }
+                    };
+                }
+            };
+        }
+
+        window_count = new_window_count;
     }
 
     let mut frequencies: HashMap<char, usize> = HashMap::new();
-    for c in current_string {
-        match frequencies.get_mut(&c) {
-            Some(v) => *v += 1,
-            _ => { frequencies.insert(c, 1); },
+    for (t, c) in window_count {
+        match frequencies.get_mut(&t.0) {
+            Some(v) => *v += c,
+            _ => {
+                frequencies.insert(t.0, c);
+            }
         };
     }
 
