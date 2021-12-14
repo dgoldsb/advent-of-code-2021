7415d57b912e1193017f92c0c9dd23014126c61f
dgoldsb <dgoldsb@live.nl>
2021-12-14 10:55:08 +0100

    Day 14, too inefficient

--- days/src/day_14.rs
+++ days/src/day_14.rs
@@ -1,11 +1,58 @@
-use aoc::parse_lines;
+use aoc::read_file;
+use std::collections::HashMap;
 
+fn parse_rules(input: &String) -> HashMap<(char, char), char> {
+    let mut rules = HashMap::new();
+    for line in input.split("\n") {
+        let mut split = line.chars();
+        rules.insert(
+            (split.nth(0).unwrap(), split.nth(0).unwrap()),
+            split.nth(4).unwrap(),
+        );
+    }
+    rules
+}
 
-fn solve(_input: &Vec<String>, _part_a: bool) -> usize {
-    0
+fn solve(template: &String, rules: &HashMap<(char, char), char>, part_a: bool) -> usize {
+    let mut current_string = template.chars().collect::<Vec<char>>();
+
+    for i in 0..40 {
+        if i == 10 && part_a {
+            break;
+        }
+
+        current_string.push('-');
+        current_string = current_string
+            .windows(2)
+            .map(|t| match rules.get(&(t[0], t[1])) {
+                Some(v) => vec![t[0], *v],
+                _ => vec![t[0]],
+            })
+            .fold(Vec::new(), |mut v, a| {
+                v.extend(a);
+                v
+            });
+
+    }
+
+    let mut frequencies: HashMap<char, usize> = HashMap::new();
+    for c in current_string {
+        match frequencies.get_mut(&c) {
+            Some(v) => *v += 1,
+            _ => { frequencies.insert(c, 1); },
+        };
+    }
+
+    frequencies.values().max().unwrap() - frequencies.values().min().unwrap()
 }
 
 pub fn day_14() -> (usize, usize) {
-    let input = parse_lines("day_14".to_string());
-    (solve(&input, true), solve(&input, false))
+    let file = read_file("day_14".to_string());
+    let mut file_split = file.split("\n\n");
+    let template = file_split.next().unwrap().to_string();
+    let rules = parse_rules(&file_split.next().unwrap().to_string());
+    (
+        solve(&template, &rules, true),
+        solve(&template, &rules, false),
+    )
 }
