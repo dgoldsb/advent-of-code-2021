883b4cdb8b0936ccd9caf109bd8a6c3f6b2e9ff5
dgoldsb <dgoldsb@live.nl>
2021-12-22 11:01:45 +0100

    Day 22 part 1

--- days/src/day_22.rs
+++ days/src/day_22.rs
@@ -1,11 +1,56 @@
+use std::collections::HashSet;
 use aoc::parse_lines;
+use aoc::ints_from_str;
+use std::str::FromStr;
 
+#[derive(Debug)]
+struct RebootStep {
+    on: bool,
+    xs: (isize, isize),
+    ys: (isize, isize),
+    zs: (isize, isize),
+}
 
-fn solve(_input: &Vec<String>, _part_a: bool) -> usize {
-    0
+impl FromStr for RebootStep {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<RebootStep, Self::Err> {
+        let on = input.contains("on");
+        let v = ints_from_str(&input.to_string());
+        let mut is = v.iter();
+        let xs = (is.next().unwrap().to_owned(), is.next().unwrap().to_owned());
+        let ys = (is.next().unwrap().to_owned(), is.next().unwrap().to_owned());
+        let zs = (is.next().unwrap().to_owned(), is.next().unwrap().to_owned());
+        return Ok(RebootStep { on, xs, ys, zs })
+    }
+}
+
+fn solve(input: &Vec<String>) -> (usize, usize) {
+    let mut set_a: HashSet<(isize, isize, isize)> = HashSet::new();
+
+    for step in input.iter().map(|s| RebootStep::from_str(s).unwrap()) {
+        // Part a, slow approach.
+        for x in step.xs.0..=step.xs.1 {
+            let part_a = x.abs() <= 50;
+            if !part_a {break;}
+            for y in step.ys.0..=step.ys.1 {
+                for z in step.zs.0..=step.zs.1 {
+                    let v = (x, y, z);
+                    if step.on {
+                        set_a.insert(v);
+                    } else {
+                        set_a.remove(&v);
+                    }
+                }
+            }
+        }
+
+        // Part b.
+    }
+    (set_a.len(), 0)
 }
 
 pub fn day_22() -> (usize, usize) {
     let input = parse_lines("day_22".to_string());
-    (solve(&input, true), solve(&input, false))
+    solve(&input)
 }
