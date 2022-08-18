08945d12db4112faef818eb70f9bfa0415cd16e9
dgoldsb <dgoldsb@live.nl>
2022-08-18 17:51:54 +0200

    Day 24 working

--- days/src/day_24.rs
+++ days/src/day_24.rs
@@ -1,160 +1,165 @@
-use aoc::parse_lines;
-use std::collections::HashMap;
+use aoc::read_file;
 use std::str::FromStr;
 
 #[derive(Debug)]
-enum Instruction {
-    Inp(char),
-    Add(char, char),
-    Mul(char, char),
-    Div(char, char),
-    Mod(char, char),
-    Eql(char, char),
+struct SubProgram {
+    a: isize,
+    b: isize,
+    truncate: bool,
 }
 
-impl FromStr for Instruction {
+impl SubProgram {
+    // Manually "decompiled".
+    fn run(&self, input: &isize, z: &isize) -> isize {
+        let mut output: isize = *z;
+
+        // Reduce z.
+        if self.truncate {
+            output /= 26;
+        }
+
+        // Increase z.
+        if ((z % 26) + self.a) != *input {
+            output *= 26;
+            output += input + self.b;
+        }
+
+        output
+    }
+
+    fn highest_option(&self, z: &isize) -> Option<isize> {
+        (1..=9).filter(|i| ((z % 26) + self.a) == *i).max()
+    }
+
+    fn lowest_option(&self, z: &isize) -> Option<isize> {
+        (1..=9).filter(|i| ((z % 26) + self.a) == *i).min()
+    }
+}
+
+impl FromStr for SubProgram {
     type Err = ();
 
-    fn from_str(input: &str) -> Result<Instruction, Self::Err> {
-        let mut split = input.split(" ");
-        let op = split.next().unwrap();
-        let val: char = split.next().unwrap().parse().unwrap();
-        match op {
-            "inp" => Ok(Instruction::Inp(val)),
-            "add" => Ok({
-                let val_val: char = split.next().unwrap().parse().unwrap();
-                Instruction::Add(val, val_val)
-            }),
-            "mul" => Ok({
-                let val_val: char = split.next().unwrap().parse().unwrap();
-                Instruction::Mul(val, val_val)
-            }),
-            "div" => Ok({
-                let val_val: char = split.next().unwrap().parse().unwrap();
-                Instruction::Div(val, val_val)
-            }),
-            "mod" => Ok({
-                let val_val: char = split.next().unwrap().parse().unwrap();
-                Instruction::Mod(val, val_val)
-            }),
-            "eql" => Ok({
-                let val_val: char = split.next().unwrap().parse().unwrap();
-                Instruction::Eql(val, val_val)
-            }),
-            _ => Err(()),
+    fn from_str(input: &str) -> Result<SubProgram, Self::Err> {
+        let lines: Vec<&str> = input.split("\n").collect();
+        if lines.len() < 16 {
+            return Err(());
         }
+
+        let a: isize = lines
+            .get(5)
+            .unwrap()
+            .split(" ")
+            .collect::<Vec<&str>>()
+            .get(2)
+            .unwrap()
+            .parse()
+            .unwrap();
+        let b: isize = lines
+            .get(15)
+            .unwrap()
+            .split(" ")
+            .collect::<Vec<&str>>()
+            .get(2)
+            .unwrap()
+            .parse()
+            .unwrap();
+        let truncate: bool = lines
+            .get(4)
+            .unwrap()
+            .split(" ")
+            .collect::<Vec<&str>>()
+            .get(2)
+            .unwrap()
+            .parse::<isize>()
+            .unwrap()
+            == 26;
+
+        return Ok(SubProgram { a, b, truncate });
     }
 }
 
-fn get_or(v: &char, memory: &HashMap<char, isize>) -> isize {
-    match memory.get(v) {
-        Some(x) => *x,
-        _ => (*v) as isize - 48, // I know
+fn run_program(program: &Vec<SubProgram>, inputs: &Vec<isize>) -> isize {
+    let mut output = 0;
+    for i in 0..14 {
+        output = program.get(i).unwrap().run(inputs.get(i).unwrap(), &output);
     }
+    output
 }
 
-// Returns w, x, y, z
-fn run_program(mut inputs: Vec<isize>, program: &Vec<Instruction>) -> (isize, isize, isize, isize) {
-    let mut memory: HashMap<char, isize> = vec![('w', 0), ('x', 0), ('y', 0), ('z', 0)]
-        .into_iter()
-        .collect::<HashMap<char, isize>>();
+fn solve_recursive(
+    maximize: bool,
+    program: &Vec<SubProgram>,
+    inputs: &mut Vec<isize>,
+    z: isize,
+) -> Result<Vec<isize>, &'static str> {
+    if (inputs.len() == 14) && run_program(program, inputs) == 0 {
+        return Ok(inputs.clone());
+    } else if inputs.len() == 14 {
+        return Err("End result did not match");
+    }
 
-    // Iterate over the program.
-    for instruction in program {
-        match instruction {
-            Instruction::Inp(a) => memory.insert(*a, inputs.remove(0)),
-            Instruction::Add(a, b) => memory.insert(*a, get_or(&a, &memory) + get_or(&b, &memory)),
-            Instruction::Mul(a, b) => memory.insert(*a, get_or(&a, &memory) * get_or(&b, &memory)),
-            Instruction::Div(a, b) => memory.insert(*a, get_or(&a, &memory) / get_or(&b, &memory)),
-            Instruction::Mod(a, b) => memory.insert(*a, get_or(&a, &memory) % get_or(&b, &memory)),
-            Instruction::Eql(a, b) => memory.insert(*a, {
-                if get_or(&a, &memory) == get_or(&b, &memory) {
-                    1
-                } else {
-                    0
-                }
-            }),
+    // If truncate is true and we cannot reduce, error out.
+    let current_index: usize = inputs.len();
+    let subprogram = program.get(current_index).unwrap();
+    if subprogram.truncate {
+        let best_choice_option = if maximize {
+            subprogram.highest_option(&z)
+        } else {
+            subprogram.lowest_option(&z)
+        };
+        match best_choice_option {
+            Some(best_choice) => {
+                inputs.push(best_choice);
+                match solve_recursive(maximize, program, inputs, subprogram.run(&best_choice, &z)) {
+                    Ok(result) => return Ok(result),
+                    Err(_) => inputs.pop(),
+                };
+            }
+            _ => return Err("No match for truncate step"),
         };
     }
 
-    (
-        *memory.get(&'w').unwrap(),
-        *memory.get(&'x').unwrap(),
-        *memory.get(&'y').unwrap(),
-        *memory.get(&'z').unwrap(),
-    )
+    // Very simple strategy, just go over range 9..=1 and hope the search space is
+    // small enough with only 7 subprograms to brute force.
+    let is = if maximize {
+        vec![9, 8, 7, 6, 5, 4, 3, 2, 1]
+    } else {
+        vec![1, 2, 3, 4, 5, 6, 7, 8, 9]
+    };
+    for i in is {
+        // Push on vector.
+        inputs.push(i);
+
+        let result = solve_recursive(maximize, program, inputs, subprogram.run(&i, &z));
+
+        match result {
+            Ok(result) => return Ok(result),
+            Err(_) => inputs.pop(),
+        };
+    }
+    Err("Ran out of options...")
 }
 
-fn solve(program: &Vec<Instruction>, _part_a: bool) -> usize {
-    // Stoopid but let's go.
-    let mut start: isize = 99999999999999;
-
-    loop {
-        // To string.
-        let current: String = start.to_string();
-
-        // Continue if has a zero.
-        if current.contains('0') {
-            continue;
-        }
-
-        // Pad.
-        let padded_current = format!("{:0>14}", current);
-
-        // Check output.
-        let input = padded_current
-            .chars()
-            .map(|c| (c as isize) - 48)
-            .collect::<Vec<isize>>();
-        let result = run_program(input, program);
-        if result.3 == 0 {
-            return start as usize;
-        }
-
-        // Minus 1.
-        start -= 1;
-    }
+fn solve(program: &Vec<SubProgram>, maximize: bool) -> usize {
+    let mut inputs = Vec::new();
+    let result = solve_recursive(maximize, program, &mut inputs, 0);
+    result
+        .unwrap()
+        .iter()
+        .map(|i| format!("{}", i))
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap()
 }
 
 pub fn day_24() -> (usize, usize) {
-    let input = parse_lines("day_24".to_string());
-    let program = input
-        .iter()
-        .map(|s| Instruction::from_str(&s).unwrap())
-        .collect::<Vec<Instruction>>();
-    (solve(&program, true), solve(&program, false))
-}
+    let input = read_file("day_24".to_string());
 
-#[cfg(test)]
-mod tests {
-    use crate::day_24::run_program;
-    use crate::day_24::Instruction;
-
-    #[test]
-    fn example_1() {
-        let result = run_program(
-            vec![1, 3],
-            &vec![
-                Instruction::Inp('z'),
-                Instruction::Inp('x'),
-                Instruction::Mul('z', '3'),
-                Instruction::Eql('x', 'z'),
-            ],
-        );
-        assert_eq!(result.3, 1);
-    }
-
-    #[test]
-    fn example_2() {
-        let result = run_program(
-            vec![1, 2],
-            &vec![
-                Instruction::Inp('z'),
-                Instruction::Inp('x'),
-                Instruction::Mul('z', '3'),
-                Instruction::Eql('x', 'z'),
-            ],
-        );
-        assert_eq!(result.3, 0);
-    }
+    let subprograms: Vec<SubProgram> = input
+        .split("inp w")
+        .map(|i| SubProgram::from_str(i))
+        .filter(|o| o.is_ok())
+        .map(|s| s.unwrap())
+        .collect::<Vec<SubProgram>>();
+    (solve(&subprograms, true), solve(&subprograms, false))
 }
